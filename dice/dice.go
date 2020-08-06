b986c4312db99fa72786a839ed26dcfbabefae1a
Neopolitan <neopolitan@mail.ustc.edu.cn>
2020-08-06 20:18:47 +0800

    debug

--- dice/dice.go
+++ dice/dice.go
@@ -48,6 +48,7 @@ func diceFun(command *bot.Cmd) (msg string, err error) {
 			}
 			result := rand.Int()%m + 1
 			diceResult += result
+			fmt.Printf("[Debug] n=%v, m=%v, result=%v\n", n, m, result)
 			submsg = strings.Join([]string{submsg, string(result)}, "")
 			if n > 1 {
 				for i := 1; i < n; i++ {
