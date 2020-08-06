9304fa73c975a1aaaf31e7f2c0d18c8c16f3045f
Neopolitan <neopolitan@mail.ustc.edu.cn>
2020-08-06 20:15:20 +0800

    dice init

--- dice/dice.go
+++ dice/dice.go
@@ -2,15 +2,72 @@ package dice
 
 import (
 	"fmt"
+	"math/rand"
+	"strconv"
+	"strings"
 
 	"github.com/NeapolitanIcecream/bot"
 )
 
-func hello(command *bot.Cmd) (msg string, err error) {
-	var message = command.Message
-	fmt.Println(message)
-	// matched, err := regexp.MatchString(`foo.*`, "seafood")
-	msg = fmt.Sprintf("Hello %s", command.User.RealName)
+const errorMessage = "Error."
+
+func diceFun(command *bot.Cmd) (msg string, err error) {
+	message := strings.TrimLeft(command.Message, "r ")
+	msgHead := fmt.Sprintf("%s roll %s: ", command.User.RealName, message)
+	msgBody := ""
+	diceResult := 0
+	dices := strings.Split(message, "+")
+
+	for _, dice := range dices {
+		numbers := strings.Split(strings.TrimSpace(dice), "d")
+		submsg := ""
+
+		switch len(numbers) {
+		case 1:
+			submsg = numbers[0]
+			n, e := strconv.Atoi(numbers[0])
+			if e != nil {
+				msg = errorMessage
+				return
+			}
+			diceResult += n
+
+		case 2:
+			n, e := strconv.Atoi(numbers[0])
+			if e != nil {
+				msg = errorMessage
+				return
+			}
+			if n > 1 {
+				submsg = strings.Join([]string{submsg, "("}, "")
+			}
+			m, e := strconv.Atoi(numbers[1])
+			if e != nil {
+				msg = errorMessage
+				return
+			}
+			result := rand.Int()%m + 1
+			diceResult += result
+			submsg = strings.Join([]string{submsg, string(result)}, "")
+			if n > 1 {
+				for i := 1; i < n; i++ {
+					result := rand.Int()%m + 1
+					diceResult += result
+					submsg = strings.Join([]string{submsg, string(result)}, "+")
+				}
+				submsg = strings.Join([]string{submsg, ")"}, "")
+			}
+
+		default:
+			msg = errorMessage
+			return
+		}
+
+		msgBody = strings.Join([]string{msgBody, submsg}, "+")
+	}
+
+	msgTail := fmt.Sprintf("=%v", diceResult)
+	msg = strings.Join([]string{msgHead, msgBody, msgTail}, "")
 	return
 }
 
@@ -21,5 +78,5 @@ func init() {
 		"r",
 		"Roll [n]d[m] dice. Example: \"!r 1d4+1d6+2\".",
 		"",
-		hello)
+		diceFun)
 }
